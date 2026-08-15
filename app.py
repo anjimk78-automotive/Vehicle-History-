bfb12c85bebf12dc6393fba3793038ca67504d94
anjimk78-automotive <anjimk78@gmail.com>
2026-08-15 10:46:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ def inject_login_style():
         overflow-y: auto;
         border: 1px solid #d6d6d6;
         border-radius: 0.8rem;
-        padding: 1.5rem 2.2rem 0.8rem 2.2rem;
+        padding: 1.5rem 2.2rem 1rem 2.2rem;
         background: rgba(255, 255, 255, 0.94);
         box-shadow: 0 12px 40px rgba(0,0,0,0.35);
     }}
